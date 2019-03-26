66bb3cb444cc7d1005d6be0709dac5cd0673ff33
you-name-it <1071476126@qq.com>
2019-03-26 11:56:41 +0800

    更新测试

--- src/test/java/test/testController.java
+++ src/test/java/test/testController.java
@@ -1,10 +1,11 @@
 package test;
 
-import com.common.utils.HttpClientUtil;
-import com.common.utils.HttpResult;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * 〈〉
@@ -12,9 +13,13 @@ import org.jsoup.nodes.Element;
  * @author HuangQiuRong
  * @create 2019/3/26
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+@WebAppConfiguration
 public class testController {
 
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
 
         HttpClientUtil httpClientUtil = new HttpClientUtil();
         HttpResult httpResult = httpClientUtil.doGet("https://toobigdata.com/douyin/user/83668?utm_source=de");
@@ -28,6 +33,18 @@ public class testController {
         //哈哈
         System.out.println(text);
 
+    }*/
+
+
+    @Before
+    public void init() {
+        System.out.println("开始测试-----------------");
     }
+
+    @After
+    public void after() {
+        System.out.println("测试结束-----------------");
+    }
+
 
 }
